e0434bcb0af54c239b9de6411749ab33fa18a376
alexanderdamiani <alexander.damiani15@gmail.com>
2021-02-16 23:08:32 -0500

    v0.0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,8 +44,9 @@ done
 # python3 -m pytest --cov-config=.coveragerc --cov=. tests/
 output=$(python3 -m pytest $pytest_cov_dirs --cov-fail-under=$3)
 
-parse_title=false
-parse_contents=false
+parse_title=false  # parsing title (not part of table)
+parse_contents=false  # parsing contents of table
+parsed_content_header=false  # finished parsing column headers of table
 output_table_title=''
 output_table_contents=''
 item_cnt=0 # four items per row in table
@@ -79,6 +80,16 @@ for x in $output; do
       continue
     else
       # parse contents
+
+      if [[ "$parsed_content_header" = false && $item_cnt = 4 ]]; then
+        # needed between table headers and values for markdown
+        output_table_contents+="
+
+        | ------ | ------ | ------ | ------ |
+        "
+        parsed_content_header=true
+      fi
+
       item_cnt=$((item_cnt % items_per_row))
 
       if [ $item_cnt = 0 ]; then
