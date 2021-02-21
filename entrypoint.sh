45829c2c153fd2de1f9a8b4517f0a1bab13bfd36
alexanderdamiani <alexander.damiani15@gmail.com>
2021-02-20 19:39:55 -0500

    added print statemets for logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,14 @@
 # --- Parameters --- #
 # $1: pytest-root-dir
 # $2: cov-ignore-dirs
-# $3: cov-threshold
+# $3: cov-threshold-single
+# $3: cov-threshold-total
+
+cov_threshold_single_fail=false
+cov_threshold_total_fail=false
+
+echo 'cov_threshold_single_fail' $cov_threshold_single_fail
+echo 'cov_threshold_total_fail' $cov_threshold_total_fail
 
 # default directories to ignore testing for coverage of .py files
 default_ignore_dirs="|
@@ -42,15 +49,18 @@ done
 
 # python3 -m pytest --cov=. tests/ --cov-fail-under=85
 # python3 -m pytest --cov-config=.coveragerc --cov=. tests/
-output=$(python3 -m pytest $pytest_cov_dirs --cov-fail-under=$3)
+output=$(python3 -m pytest $pytest_cov_dirs)
 
 parse_title=false  # parsing title (not part of table)
 parse_contents=false  # parsing contents of table
 parsed_content_header=false  # finished parsing column headers of table
-output_table_title=''
-output_table_contents=''
 item_cnt=0 # four items per row in table
 items_per_row=4
+
+output_table_title=''
+output_table_contents=''
+file_covs=()
+total_cov=0
 
 for x in $output; do
   if [ "$x" = "----------------------------------------" ]; then
@@ -82,13 +92,21 @@ for x in $output; do
       # parse contents
 
       if [[ "$parsed_content_header" = false && $item_cnt = 4 ]]; then
-        # needed between table headers and values for markdown
+        # needed between table headers and values for markdown table
         output_table_contents+="
 | ------ | ------ | ------ | ------ |"
-        parsed_content_header=true
+      fi
+
+      if [[ $item_cnt = 3 ]]; then
+        # store individual file coverage
+        echo $x
+        file_covs+=( ${x::-1} )  # remove percentage at end
+        total_cov=${x::-1}  # will store last one
       fi
 
       item_cnt=$((item_cnt % items_per_row))
+
+      parsed_content_header=true
 
       if [ $item_cnt = 0 ]; then
         output_table_contents+="
@@ -107,6 +125,33 @@ for x in $output; do
   output_table+="$x"
 done
 
+# remove last file-cov b/c it's total-cov
+unset 'file_covs[${#file_covs[@]}-1]'
+
+# remove first file-cov b/c it's table header
+file_covs=("${file_covs[@]:1}") #removed the 1st element
+
+for a in "${file_covs[@]}"; do echo "$a"; done
+echo 'total_cov' $total_cov
+
+echo 'cov_threshold_single_fail' $cov_threshold_single_fail
+echo 'cov_threshold_total_fail' $cov_threshold_total_fail
+
+# check if any file_cov exceeds threshold
+for file_cov in "${file_covs[@]}"; do
+  if [ "$file_cov" -lt $3 ] ; then
+    cov_threshold_single_fail=true
+  fi
+done
+
+# check if total_cov exceeds threshold
+if [ "$total_cov" -lt $4 ];
+  then cov_threshold_total_fail=true
+fi
+
+echo 'cov_threshold_single_fail' $cov_threshold_single_fail
+echo 'cov_threshold_total_fail' $cov_threshold_total_fail
+
 echo $output_table_title
 # echo $output_table
 echo $output_table_contents
@@ -118,7 +163,5 @@ output_table_contents="${output_table_contents//$'\n'/'%0A'}"
 output_table_contents="${output_table_contents//$'\r'/'%0D'}"
 
 echo "::set-output name=output-table::$output_table_contents"
-
-time1=$(date)
-echo "::set-output name=time::$time1"
-echo "::set-output name=time1::$time1"
+echo "::set-output name=cov-threshold-single-fail::$cov_threshold_single_fail"
+echo "::set-output name=cov-threshold-total-fail::$cov_threshold_total_fail"
