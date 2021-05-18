858ddeb703601d81ea35cd92d670000464ba8fc0
alexanderdamiani <alexander.damiani15@gmail.com>
2021-05-17 21:21:31 -0400

    hard code dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,9 +35,9 @@ done
 
 echo $pytest_cov_dirs
 
-echo "python3 -m pytest $pytest_cov_dirs --cov-config=.coveragerc $2"
+echo "python3 -m pytest $1 --cov-config=.coveragerc $2"
 
-output=$(python3 -m pytest $pytest_cov_dirs --cov-config=.coveragerc $2)
+output=$(python3 -m pytest $1 --cov-config=.coveragerc $2)
 
 # remove pytest-coverage config file
 if [ -f $cov_config_fname ]; then
