275098cc7a441d12035c37038c61ed50268ce034
Kyle Chou <kylechou@devopsthink.org>
2021-06-22 17:12:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,9 @@ for dir in $pytest_dirs; do
   pytest_cov_dirs+="--cov=${dir} "
 done
 
-output=$(python3 -m pytest $pytest_cov_dirs --cov-config=.coveragerc $2)
+# output=$(python3 -m pytest $pytest_cov_dirs --cov-config=.coveragerc $2)
+python3 -m pytest --cov .
+output=$(python3 -m pytest --cov .)
 
 # remove pytest-coverage config file
 if [ -f $cov_config_fname ]; then
